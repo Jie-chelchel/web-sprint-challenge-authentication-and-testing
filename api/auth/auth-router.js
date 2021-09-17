c6c313e5195657353b8f5fd03d059bc4d0c54380
Jie Zhang <zhangjiew@gmail.com>
2021-09-16 23:55:03 -0600

    restricted middleware done

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -42,24 +42,24 @@ router.post("/register", validateInput, usernameExist, (req, res, next) => {
   */
 });
 
-// router.post("/login", validateInput, (req, res, next) => {
-//   let { username, password } = req.body;
-//   Users.findBy({ username })
-//     .then(([user]) => {
-//       if (user && bcrypt.compareSync(password, user.password)) {
-//         const token = tokenBuilder(user);
-//         res.status(200).json({
-//           message: `welcome, ${user.username}`,
-//           token,
-//         });
-//       } else {
-//         res.status(401).json({ message: "invalid credentials" });
-//       }
-//     })
-//     .catch((err) => {
-//       next(err);
-//     });
-/*
+router.post("/login", validateInput, (req, res, next) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .then(([user]) => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = tokenBuilder(user);
+        res.status(200).json({
+          message: `welcome, ${user.username}`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "invalid credentials" });
+      }
+    })
+    .catch((err) => {
+      next(err);
+    });
+  /*
     IMPLEMENT
     You are welcome to build additional middlewares to help with the endpoint's functionality.
 
@@ -82,6 +82,6 @@ router.post("/register", validateInput, usernameExist, (req, res, next) => {
     4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
       the response body should include a string exactly as follows: "invalid credentials".
   */
-// });
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -16,8 +16,8 @@ server.use(express.json());
 server.use("/api/auth", authRouter);
 server.use("/api/jokes", restrict, jokesRouter); // only logged-in users should have access!
 
+// eslint-disable-next-line
 server.use((err, req, res, next) => {
-  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
